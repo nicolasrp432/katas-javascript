123515f58d91ce022c12fd159e29cbfc4d6a4b63
Imanol Valero <imanol.valero@live.com>
2025-04-11 11:42:19 +0200

    fix[vite config]: corregir patrón de tests/include

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
-    include: ['src']
+    include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
   },
 });
